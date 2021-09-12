30f56bea46d7b6c83bf1a41c6c2de4eb26f0580b
snayineni devops <snayineni@gmail.com>
2021-09-12 19:22:04 -0400

    master

--- TerraformBasicsPractice/createinstances.tf
+++ TerraformBasicsPractice/createinstances.tf
@@ -1,10 +1,9 @@
-provider "aws" {
-  access_key = "Aceess key here"
-  secret_key = "Secret key here"
-  region = "us-east-2"
-}
 
 resource "aws_instance" "Myfirstinstance" {
-  ami = "ami-fefecdfdfe"
-  instance_type =  = "t2.micro"  
+  ami = "ami-05692172625678b4e"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Name" = "Threedemoinstances"
+  }
 }
